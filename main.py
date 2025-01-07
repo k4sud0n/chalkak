f3c6cf83fb6ddf9ab6f394be6d94a37128201472
Sihyun Kim <k4sud0n@gmail.com>
2025-01-07 14:14:29 +0900

    fix location

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def main_page(request: Request):
+async def main(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-# 일원터널
-@app.get("/location/irwon-tunnel", response_class=HTMLResponse)
-async def get_overlay(request: Request):
+@app.get("/location/{header_url_code}/{location_code}", response_class=HTMLResponse)
+async def get_overlay(request: Request, header_url_code: str, location_code: str):
     return templates.TemplateResponse(
-        "overlay.html", {"request": request, "code": "238"}
+        "overlay.html",
+        {
+            "request": request,
+            "header_url_code": header_url_code,
+            "location_code": location_code,
+        },
     )
